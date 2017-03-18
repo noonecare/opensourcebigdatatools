3c9e24edae78b62ae8c000b2c5ac86466b09e09b
王蒙 <wangmeng@gridsum.com>
2017-03-18 13:51:11 +0800

    接受 stdin 输入，对于每个输入计算 sigmoid 函数值，把只打印到 stdout 上

---
+++ HiveScripts/sigmoid.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+import sys
+import math
+
+
+# 机器学习中常用的 sigmoid 函数
+def sigmoid(x):
+    return 1 / (1 + math.exp(x))
+
+for line in sys.stdin:
+    # hive 输出结果默认是以 \t 区分各个列的
+    print("\t".join([str(sigmoid(field)) for field in line.split("\t")]))
